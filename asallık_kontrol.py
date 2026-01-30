7c7fa80c2be9a23cd10adcb0e580480a78fece69
itu-itis25-kocagobek24 <kocagobek24@itu.edu.tr>
2026-01-30 18:16:37 +0300

    Update asallık_kontrol.py

--- asallık_kontrol.py
+++ asallık_kontrol.py
@@ -2,18 +2,16 @@ x = input("enter the integer: ")
 liste1 = []
 for i in x:
     if not i in "1234567890":
-        print("this is not an integer! please, enter an integer")
-        x = 0
-        break
-if x == 0:
-    print("try again")
+        raise ValueError("this is not an integer")
+s = int(x)
+if s =< 1:
+    print("you should enter an approvable number")
 else:       
-    s = int(x)
-    
     for a in range(2,s):
         if s % a == 0:
             print(f"{x} bir asal sayı değildir") 
             liste1.append(s)
             break
     if liste1 == []:
+
         print(f"{x} bir asal sayıdır")       
